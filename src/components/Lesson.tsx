3c06a2b1eb81cecd9cb5935fbe46514df5f0add8
Nathanael <dev@nathabonfim59.com>
2022-06-25 23:23:04 -0300

    feat(components/lesson): added props and data fetching

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,24 +1,53 @@
-import { CheckCircle } from 'phosphor-react'
-export function Lesson() {
+import { format, isPast } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+import { CheckCircle, Lock } from 'phosphor-react'
+
+interface LessonProps {
+  id: string
+  title: string
+  slug: string
+  availableAt: Date
+  type: 'class' |  'live'
+}
+
+export function Lesson(props: LessonProps) {
+  const isLessonAvailable = isPast(props.availableAt)
+  const availableDateFormatted = format(
+    props.availableAt,
+    "EEEE' • ' d ' de 'MMMM' • 'HH'h'mm",
+    { locale: ptBR }
+  )
+
+  function captalizeFirstLetter(str: string) {
+    return str.replace(/^./, str[0].toUpperCase())
+  }
+
   return (
     <div className="flex flex-col gap-2">
       <span className="text-gray-300">
-        Domingo • 20 de junho • 19h00
+        { captalizeFirstLetter(availableDateFormatted) }
       </span>
 
       <div className="p-4 rounded border border-gray-600 flex flex-col gap-4">
         <header className="text-sm font-medium flex h-6">
-          <span className="flex flex-1 text-blue-500 items-center">
-            <CheckCircle size={20} className="mr-2"/>
-            Conteúdo liberado
-          </span>
+          { isLessonAvailable ? (
+            <span className="flex flex-1 text-blue-500 items-center">
+              <CheckCircle size={20} className="mr-2"/>
+              Conteúdo liberado
+            </span>
+          ) : (
+            <span className="flex flex-1 text-orange-500 items-center">
+              <Lock size={20} className="mr-2"/>
+              Em breve
+            </span>
+          )}
 
           <span className="rounded border border-green-300 py-[0.125rem] px-2 text-xs h-[23px] flex items-center">
-            AO VIVO
+            { props.type == "live" ? "AO VIVO" : "AULA PRÁTICA" }
           </span>
         </header>
         <strong className="text-base">
-          Abertura do evento Ignite labs
+          { props.title }
         </strong>
       </div>
     </div>
